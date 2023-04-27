d88bfd8aa4b9a8365977904d98527eab3d09f925
Simon Heimlicher <sh@srvr.ch>
2023-04-27 08:41:05 +0200

    Adapt to changes in theme claris

--- assets/scripts/vitae-body.js
+++ assets/scripts/vitae-body.js
@@ -1,5 +1,5 @@
-// console.log('BEGIN vitae/modern_late.js');
+// console.log('BEGIN vitae-body');
 import 'scripts/vitae/theme/obfuscated-link';
 import 'scripts/vitae/theme/qrcode-svg';
 import 'scripts/vitae/theme/print-view';
-// console.log('END   vitae/modern_late.js');
+// console.log('END   vitae-body');
